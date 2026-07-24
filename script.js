e07cc35c66844efbe9a991d93ee1e53b185b2a32
Princerao12 <140870254+Princerao12@users.noreply.github.com>
2026-07-25 03:44:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,105 @@
+let list = [];
+
+const container = document.getElementById("listContainer");
+const status = document.getElementById("status");
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function drawList(highlight = -1) {
+    container.innerHTML = "";
+
+    list.forEach((value, index) => {
+        const node = document.createElement("div");
+        node.className = "node";
+        node.innerText = value;
+
+        if (index === highlight) {
+            node.style.background = "#ef4444";
+        }
+
+        container.appendChild(node);
+    });
+}
+
+async function insertBegin() {
+    let value = document.getElementById("value").value;
+
+    if (value === "") return alert("Enter a value");
+
+    status.innerText = "Creating node...";
+    await sleep(500);
+
+    list.unshift(value);
+
+    status.innerText = "Inserted at Beginning";
+    drawList();
+}
+
+async function insertEnd() {
+    let value = document.getElementById("value").value;
+
+    if (value === "") return alert("Enter a value");
+
+    status.innerText = "Creating node...";
+    await sleep(500);
+
+    list.push(value);
+
+    status.innerText = "Inserted at End";
+    drawList();
+}
+
+async function deleteBegin() {
+    if (list.length === 0) return;
+
+    status.innerText = "Deleting first node...";
+    await sleep(500);
+
+    list.shift();
+
+    status.innerText = "Deleted";
+    drawList();
+}
+
+async function deleteEnd() {
+    if (list.length === 0) return;
+
+    status.innerText = "Deleting last node...";
+    await sleep(500);
+
+    list.pop();
+
+    status.innerText = "Deleted";
+    drawList();
+}
+
+async function searchNode() {
+    let value = document.getElementById("value").value;
+
+    if (value === "") return;
+
+    status.innerText = "Searching...";
+
+    for (let i = 0; i < list.length; i++) {
+        drawList(i);
+        await sleep(700);
+
+        if (list[i] == value) {
+            status.innerText = "Found at Position " + (i + 1);
+            return;
+        }
+    }
+
+    drawList();
+    status.innerText = "Not Found";
+}
+
+function resetList() {
+    list = [];
+    drawList();
+    status.innerText = "List Reset";
+}
+
+drawList();
